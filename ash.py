ef7628032dad75c4ac18b2095ad60d90ce7e6cbb
arash-nm <arashnm797@gmail.com>
2025-03-27 18:11:24 +0330

    auto run without python3 comment

--- ash.py
+++ ash.py
@@ -1,24 +1,27 @@
+#!/usr/bin/env python3
+
 import os
 import sys
+import readline
 
 def run_shell():
     while True:
-        # نمایش پرامپت
+        # Display the prompt
         user_input = input(f"{os.getcwd()}$ ")
 
-        # اگر ورودی "exit" باشد، از شل خارج می‌شود
+        # If the input is "exit", exit the shell
         if user_input.strip().lower() == "exit":
             print("Exiting shell...")
             break
 
-        # اگر ورودی دستور shell باشد، آن را اجرا می‌کند
+        # If the input is a "cd" command, change the directory
         elif user_input.startswith("cd "):
             try:
-                os.chdir(user_input[3:].strip())  # تغییر دایرکتوری
+                os.chdir(user_input[3:].strip())  # Change the directory
             except FileNotFoundError:
                 print(f"Directory not found: {user_input[3:].strip()}")
         else:
-            # اجرای دستورات دیگر
+            # Execute other commands
             os.system(user_input)
 
 if __name__ == "__main__":
